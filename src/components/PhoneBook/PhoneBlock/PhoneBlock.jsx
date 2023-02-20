aa7273fa4b5a6ee2951245df9368a9db34075009
Anton Orlov <anton_orlov@ukr.net>
2023-02-20 11:13:02 +0200

    update work

---
+++ src/components/PhoneBook/PhoneBlock/PhoneBlock.jsx
@@ -0,0 +1,12 @@
+import css from './phone-block.module.scss';
+
+const PhoneBlock = ({ title, children }) => {
+  return (
+    <div className={css.wrapper}>
+      <h3 className={css.title}>{title}</h3>
+      {children}
+    </div>
+  );
+};
+
+export default PhoneBlock;
